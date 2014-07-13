d084ff29fe0ea12f2e5d027acb04db1bf8017e6d
Federico Tomassetti <f.tomassetti@gmail.com>
2014-07-13 09:20:58 +0100

    First temptative to use clj-antlr

--- project.clj
+++ project.clj
@@ -1,3 +1,12 @@
-(defproject app "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+(defproject app "0.0.1-SNAPSHOT"
+  :description "A Java formatter and beautifier"
+  :dependencies [
+  		[org.clojure/clojure "1.6.0"]
+  		[clj-antlr "0.2.2"]
+  	]
+  :plugins [
+  	[lein-antlr4 "0.1.0-SNAPSHOT"]]
+  :antlr-src-dir "src/antlr"
+  :antlr-dest-dir "gen-src"
+  :antlr-options {:verbose true}
+  :main app.core)
--- src/app/core.clj
+++ src/app/core.clj
@@ -1,1 +1,8 @@
-(ns app.core)
+(ns app.core
+	(:require [clj-antlr.core :as antlr])
+	(:require [clojure.pprint :as pp]))
+
+
+(defn -main []
+	(def java (antlr/parser "src/antlr/Java.g4"))
+	(pp/pprint (java "package a; class A { }")))
